7eebc7dd594b20ef4c641a919c266dd9d4da493e
georg-x <georg.hoellrigl@gmx.at>
2025-03-26 13:29:58 +0100

    basic script, missing checks

--- mxtossl.sh
+++ mxtossl.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Todo: check for prerequisites ... make / ar?
+
 # Suche openssl*.tar.gz Files
 ls -1 *.tar.gz | while read LINE; do 
     echo $LINE
-    #openssl-3.5.0-beta1.tar.gz
+    #LINE=openssl-3.5.0-beta1.tar.gz
     XPATH=${LINE%%.tar.gz}
     VERSION=${XPATH##openssl-}
     rm -rf $XPATH
@@ -11,8 +13,17 @@ ls -1 *.tar.gz | while read LINE; do
     cd $XPATH
     ./config
     make
-    
-done
+    cd $XPATH
+    # teste binary
+    cd apps
+    ./openssl.exe version
+    mkdir bin
+    cp -a openssl.exe bin/openssl3.exe
+    cp cygcrypto-3.dll bin
+    cp cygssl-3.dll bin
+    zip ${XPATH}.mxt3 bin/*
+    mv ${XPATH}.mxt3 ../../
+done 
 
 # Entpacke alle diese Files
 # config
